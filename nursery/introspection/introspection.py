78babb5ba76272c52e8a5761386b690fa12dce56
Ryan Hodges <ryan.d.hodges@gmail.com>
2019-07-10 13:08:17 -0700

    I think this is just different line endings

--- nursery/introspection/introspection.py
+++ nursery/introspection/introspection.py
@@ -3,11 +3,10 @@ import importlib
 def get_class(path):
     """Given a dotted path to a class, returns a class object.
     If the class doesn't exist, it variously raises ValueError or AttributeError
-    
-    TODO: Examine the use cases here, and if it's still needed, at least 
-    refactor it to return None if the class doesn't exist. 
+
+    TODO: Examine the use cases here, and if it's still needed, at least
+    refactor it to return None if the class doesn't exist.
     """
     module, dot, cls = path.rpartition('.')
     m = importlib.import_module(module)
     return m.__getattribute__(cls)
-
